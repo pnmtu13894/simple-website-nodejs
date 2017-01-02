e51351b25d3364e4a6181923d9c2976ba3856009
minhtu <pnmtu13894@gmail.com>
2017-01-02 19:52:40 +0700

    simple-website

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+module.exports = {User};
---
+++ server/server.js
@@ -0,0 +1,182 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const {ObjectID} = require('mongodb');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./mongodb/mongodb-connect');
+var {Book} = require('./models/books');
+var {User} = require('./models/user');
+var multer = require('multer');
+var app = express();
+var port = process.env.port || 3000;
+var crypto = require('crypto');
+var fs = require('fs');
+
+hbs.registerPartials(path.join(__dirname, '../views/partials'));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + './../public'));
+
+// Define a destination for uploaded image
+var storage = multer.diskStorage({
+  destination: 'public/img/',
+  // Decrypt the image to be seeable
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
+
+      cb(null, raw.toString('hex') + path.extname(file.originalname))
+    })
+  }
+});
+
+// Define a storage of the image path
+var upload = multer({storage: storage});
+
+// Define a helper function by handlebars
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+// Define a helper function by handlebars
+hbs.registerHelper('makeUppercase', (message) =>{
+  return message.toUpperCase();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/createBook', (req, res) => {
+   Book.find().sort({_id: -1}).then((books) => {
+    res.render('createBook.hbs', {
+      cBooks: books,
+      pageTitle: 'Create New Book'
+    });
+  }, (e) => {
+    console.log('Unable to fetch data');
+  });
+});
+
+app.post('/createBook', upload.single('image'), (req, res) => {
+  console.log(req.file);
+  var book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    type: req.body.type,
+    price: req.body.price,
+    image: req.file.filename
+    });
+
+  book.save().then((cbook) => {
+    console.log('Saved book');
+    res.redirect('/createBook');
+  }, (e) => {
+    console.log('Unable to save', e);
+  });
+
+
+});
+
+app.get('/', (req, res) => {
+  res.render('homepage.hbs', {
+    pageTitle: "Home Page",
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/test', (req, res) => {
+  Book.find().then((books) => {
+    res.render('test.hbs', {
+      cBooks: books,
+      pageTitle: 'Test page',
+    });
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About page');
+});
+
+app.post('/book/delete', (req, res) => {
+  // get the id
+  var id = req.body.id;
+  //validate the id
+  console.log(id);
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  //remove by id
+  Book.findByIdAndRemove(id).then((book) => {
+    imagePath = path.join(path.join(path.join(__dirname, 'public'), 'img'), book.image);
+    console.log(imagePath);
+    if(!book){
+      return res.status(404).send();
+    }
+    fs.exists(path.join('public/img', book.image), (image) => {
+      if(image){
+        console.log('File exists');
+      } else{
+        console.log('File is not found');
+      }
+    })
+    fs.unlink(path.join('public/img', book.image), (err) => {
+      if(err){
+        console.log('Unable to delete image')
+      }
+    });
+    res.redirect('/createBook');
+  }).catch((e) => {
+    console.log('Unable to delete item');
+    res.status(404).send();
+  });
+});
+
+app.get('/Book/update/:id', (req, res) => {
+  var id = req.params.id;
+
+  console.log(id);
+
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+
+  Book.findById({
+    _id: new ObjectID(id)
+  }).then((cBook) => {
+    res.render('updateBook.hbs', {
+      currentBook: cBook,
+      pageTitle: 'Update book'
+    });
+  }, (e) => {
+    console.log('Unable to fetch data');
+  });
+});
+
+app.post('/Book/update', (req, res) => {
+  var id = req.body.id;
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+
+  Book.findByIdAndUpdate(id,{
+    _id: id,
+    title: req.body.title,
+    author: req.body.author,
+    type: req.body.type,
+    time: new Date(),
+    price: req.body.price
+  }).then((cbook) => {
+    res.redirect('/createBook');
+  }, (e) => {
+    console.log('Unable to fetch data');
+  });
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`The server is on port ${port}`);
+});
